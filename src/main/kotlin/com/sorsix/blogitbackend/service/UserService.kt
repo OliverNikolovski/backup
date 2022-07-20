96fb72aa50ac9921eff58c5c1d11a419444f47fa
Vasko Bozinovski <vbozinovski16@gmail.com>
2022-07-20 15:55:43 +0200

    nothing

--- src/main/kotlin/com/sorsix/blogitbackend/service/UserService.kt
+++ src/main/kotlin/com/sorsix/blogitbackend/service/UserService.kt
@@ -4,6 +4,5 @@ import com.sorsix.blogitbackend.model.User
 import com.sorsix.blogitbackend.model.enumeration.Role
 
 interface UserService {
-//    fun register(username: String, password: String, repeatPassword: String, email: String, shortBio: String, profilePicture: ,  role: Role
-//    ): User
+
 }
