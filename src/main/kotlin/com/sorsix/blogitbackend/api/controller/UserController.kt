1b195dbd2bbd795d16177cb86b949cfac8cb5a01
Vasko Bozinovski <vbozinovski16@gmail.com>
2022-07-26 10:50:15 +0200

    .

--- src/main/kotlin/com/sorsix/blogitbackend/api/controller/UserController.kt
+++ src/main/kotlin/com/sorsix/blogitbackend/api/controller/UserController.kt
@@ -4,7 +4,6 @@ import com.sorsix.blogitbackend.model.Blog
 import com.sorsix.blogitbackend.service.BlogService
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
